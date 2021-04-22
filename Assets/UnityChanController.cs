dd54722c73cd811fad2c8e22a5146fd30aa561ac
atsushiokumura-oss <81137612+atsushiokumura-oss@users.noreply.github.com>
2021-04-22 18:18:20 +0900

    Initial commit

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    private Rigidbody myRigidbody;
+
+    private float velocityZ = 16f;
+
+    private float velocityX = 10f;
+
+    private float velocityY = 10f;
+
+    private float movableRange = 3.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myAnimator = GetComponent<Animator>();
+
+        this.myAnimator.SetFloat ("Speed", 1);
+
+        this.myRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float InputVelocityX = 0;
+
+        float InputVelocityY = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
+        {
+            InputVelocityX = -this.velocityX;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && this.movableRange > this.transform.position.x)
+        {
+            InputVelocityX = this.velocityX;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            this.myAnimator.SetBool("Jump", true);
+            InputVelocityY = velocityY;
+        }
+
+        else
+        {
+            InputVelocityY = this.myRigidbody.velocity.y;
+        }
+
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+
+        this.myRigidbody.velocity = new Vector3(InputVelocityX, InputVelocityY, this.velocityZ);
+    }
+}
